f47c3c2375b01f0c736d3ebc436e936176e72fa6
Lidii <lidiiany92@gmail.com>
2020-05-05 13:36:11 -0700

    added post test

---
+++ blog/tests/unit/post.test.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+from post import Post
+
+
+class PostTest(TestCase):
+    def test_create_post(self):
+        p = Post('Test', 'Test Content')
+
+        self.assertEqual('Test', p.title)
+        self.assertEqual('Test Content', p.content)
