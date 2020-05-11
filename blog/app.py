35d5bd08236a5dd1b9f8875d4ff09104125e023a
Lidii <lidiiany92@gmail.com>
2020-05-11 12:00:29 -0700

    patching the input field

--- blog/app.py
+++ blog/app.py
@@ -1,8 +1,11 @@
 blogs = dict()
 
+MENU_PROMPT = '\nEnter "c" to create a blog, "l" to list them, "r" to read one, "p" to write a post, or "q" to quit: '
+
 def menu():
 
     print_blogs()
+    selection = input(MENU_PROMPT)
 
 def print_blogs():
     for key, blog in blogs.items():
--- blog/tests/system/app_test.py
+++ blog/tests/system/app_test.py
@@ -5,6 +5,20 @@ import app
 from blog import Blog
 
 class App(TestCase):
+
+    def test_menu_prints_prompt(self):
+        with patch('builtins.input') as mocked_input:
+            app.menu()
+            mocked_input.assert_called_with(app.MENU_PROMPT)
+
+    def  test_menu_calls_print_blogs(self):
+        with patch('app.print_blogs') as mocked_print_blogs:
+            with patch('builtins.input', return_value='q'):
+                app.menu()
+                mocked_print_blogs.assert_called()
+
+
+
     def test_print_blogs(self):
         blog = Blog('Test', 'Test Author')
         app.blogs = {'Test': blog}
