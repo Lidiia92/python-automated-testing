e055b3ea5c7743dceddc36aa4311f135e90ea076
Lidii <lidiiany92@gmail.com>
2020-05-12 12:30:32 -0700

    added test_print_posts

--- blog/tests/system/app_test.py
+++ blog/tests/system/app_test.py
@@ -3,6 +3,7 @@ from unittest.mock import patch
 
 import app
 from blog import Blog
+from post import Post
 
 class App(TestCase):
 
@@ -25,3 +26,43 @@ class App(TestCase):
         with patch('builtins.print') as mocked_print:
             app.print_blogs()
             mocked_print.assert_called_with('- Test by Test Author (0 posts)')
+
+    def test_ask_create_blog(self):
+        with patch('builtins.input') as mocked_input:
+            mocked_input.side_effect = ('Test', 'Author')
+
+            app.ask_create_blog()
+
+            self.assertIsNotNone(app.blogs.get('Test'))
+            self.assertEqual(app.blogs.get('Test').title, 'Test')
+            self.assertEqual(app.blogs.get('Test').author, 'Author')
+
+    def test_ask_read_blog(self):
+        blog = Blog('Test', 'Test Author')
+        app.blogs = {'Test': blog}
+        with patch('builtins.input', return_value='Test'):
+            with patch('app.print_posts') as mocked_print_posts:
+                app.ask_read_blog()
+
+                mocked_print_posts.assert_called_with(blog)
+
+    def test_print_post(self):
+        blog = Blog('Test', 'Test Author')
+        app.blogs = {'Test': blog}
+        blog.create_post('Test Post', 'Test Content')
+        with patch('app.print_post') as mocked_print_post:
+            app.print_posts(blog)
+
+            mocked_print_post.assert_called_with(blog.posts[0])
+
+    def test_print_post(self):
+        post = Post('Post title', 'Post content')
+        expected_print = """
+        --- Post title ---
+        Post content
+        """
+
+        with patch('builtins.print') as mocked_print:
+            app.print_post(post)
+
+            mocked_print.assert_called_with(expected_print)
